787328262a28ee977b4edef402016ecfc17fa1df
AbdullahAbdulHannan <121689985+AbdullahAbdulHannan@users.noreply.github.com>
2023-04-07 10:38:49 +0500

    upload

---
+++ app.js
@@ -0,0 +1,149 @@
+/*++++++++++++++++++++++++++++++++++ASSIGNMENT:1+++++++++++++++++++++++++++++++++++++++++++*/
+                  
+/*===================================QUESTION 1============================================*/
+
+// let num1=+prompt("ENTER FIRST NUMBER:");
+// let num2=+prompt("ENTER SECOND NUMBER:");
+// if(num1>num2){
+//     console.log(num1);
+// }
+// else{
+//     console.log(num2);
+// }
+
+/*===================================QUESTION 2============================================*/
+
+//  let number=+prompt("ENTER A NUMBER:");
+//  if(number>0){
+//     alert("The sign is +");
+    
+//  }
+//  else if(number<0){
+//     alert("The sign is -");
+//  }
+//  else if(number==0){
+//     alert("The number is 0");
+//  }
+//  else{
+//     alert("It seems you have not write a number");
+//  }
+
+/*===================================QUESTION 3============================================*/
+
+// let n1=+prompt("ENTER FIRST NUMBER:");
+// let n2=+prompt("ENTER SECOND NUMBER:");
+// let n3=+prompt("ENTER THIRD NUMBER:");
+// let n4=+prompt("ENTER FOURTH NUMBER:");
+// let n5=+prompt("ENTER FIFTH NUMBER:");
+// if(n1>n2 && n1>n3 && n1>n4 && n1>n5){
+//     console.log(n1);
+// }
+// else if(n2>n1 && n2>n3 && n2>n4 && n2>n5){
+//     console.log(n2);
+// }
+// else if(n3>n1 && n3>n2 && n3>n4 && n3>n5){
+//     console.log(n3);
+// }
+// else if(n4>n1 && n4>n2 && n4>n3 && n4>n5){
+//     console.log(n4);
+// }
+// else if(n5>n1 && n5>n2 && n5>n3 && n5>n4){
+//     console.log(n5);
+// }
+// else{
+//     console.log("YOU MIGHT ENTERING SOME OR ALL NUMBERS EQUAL ");
+// }
+
+/*===================================QUESTION 4============================================*/
+
+// for(i=0;i<=15;i++){
+//     if(i%2==0){
+//         console.log(i+" is even\n");
+//     }
+//     else{
+//         console.log(i+" is odd\n");
+//     }
+// }
+
+/*===================================QUESTION 5============================================*/
+
+
+
+// let obtainedNumbers=+prompt("ENTER YOUR OBTAINED NUMBERS:");
+// let total_Marks=100;
+// let percentage=(obtainedNumbers/total_Marks)*100;
+// if(percentage<=100 && percentage>90){
+//          console.log("YOUR GRADE IS----------------A");
+// }
+// else if(percentage<=90 && percentage>80){
+//          console.log("YOUR GRADE IS----------------B");
+// }
+// else if(percentage<=80 && percentage>70){
+//          console.log("YOUR GRADE IS----------------C");
+// }
+// else if(percentage<=70 && percentage>60){
+//          console.log("YOUR GRADE IS----------------D");
+// }
+// else if(percentage<=60 && percentage>50){
+//          console.log("YOUR GRADE IS----------------F");
+// }
+// else{
+//    console.log ("YOU ARE FAIL");
+// }
+
+
+
+
+
+
+/*===================================QUESTION 6============================================*/
+
+// for(i=1;i<=100;i++){
+//         if(i%3==0 && i%5!=0){
+//             console.log( "Fizz\n");
+//         }
+//         else if(i%5==0 && i%3 !=0){
+//             console.log("Buzz\n");
+//         }
+//         else if(i%3==0 && i%5==0){
+//             console.log("FizzBuzz\n");
+//         }
+//         else{
+//     console.log(i);
+//         }
+//     }
+
+/*===================================QUESTION 7============================================*/
+
+// for(i=1;i<=5;i++){
+//     for(j=1;j<=i;j++){
+//         document.write(" * ");
+//     }
+//     document.write("<br>");
+// }
+
+
+
+/*################################CLASS: 03 TASK######################################*/
+
+/*===================================TABLE ASSIGNMENT============================================*/
+
+// let tableNum=+prompt("Enter table number:");
+// let startNum=+prompt("Enter start number:");
+// let endNum=+prompt("Enter end number or limit of table: ");
+// for(i=startNum;i<=endNum;i++){
+//     result=tableNum*i
+//     document.write("<center>"+ tableNum +" x "+ i +" = "+ result +" <br> </center>")
+// }
+
+/*================================IF-ELSE ASSIGNMENT==========================================*/
+
+// let age=+prompt("ENTER YOUR AGE :");
+// if(age<=10){
+//     alert("YOU ARE WELCOMED");
+// }
+// else{
+//     alert("YOU ARE NOT ALLOWED");
+// }
+
+/*===================================THE END============================================*/
